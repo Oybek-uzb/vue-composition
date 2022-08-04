24d849398ef7aed9d1f4b3d79207aabbf8bf5c96
Oybek-uzb <78894838+Oybek-uzb@users.noreply.github.com>
2022-08-04 16:35:41 +0500

    errors logic added

--- src/use/field.js
+++ src/use/field.js
@@ -1,9 +1,19 @@
-import {ref} from "vue";
+import {reactive, ref, watch} from "vue";
 
 export function useField(field) {
     const valid = ref(true)
     const value = ref(field.value)
-    // const validators
+    const errors = reactive({})
 
-    return {value, valid}
+    const reassign = val => {
+        Object.keys(field.validators ?? {}).map(name => {
+            const isValid = field.validators[name](val)
+            errors[name] = !isValid
+        })
+    }
+
+    watch(value, reassign)
+    reassign(field.value)
+
+    return {value, valid, errors}
 }
