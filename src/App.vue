0edfb8862625be33e8734ea9de0edcb4c8e7ff7e
Oybek-uzb <78894838+Oybek-uzb@users.noreply.github.com>
2022-08-04 15:46:22 +0500

    start working with validation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <form class="card">
+    <form class="card" @submit.prevent="submit">
       <h1>Auth</h1>
 
       <div class="form-control">
@@ -13,13 +13,35 @@
         <input type="password" id="password">
       </div>
 
-      <button class="btn primary">Submit</button>
+      <button class="btn primary" type="submit">Submit</button>
     </form>
   </div>
 </template>
 
 <script>
-  export default {
+  import {useForm} from "@/use/form";
 
+  const required = val => !!val
+  const minLength = num => val => val.length >= num
+
+  export default {
+    setup() {
+      const form = useForm({
+        email: {
+          value: 'my@email.uz',
+          validators: {required}
+        },
+        password: {
+          value: 'myPassword',
+          validators: {required, minLength: minLength(8)}
+        }
+      })
+
+      function submit() {
+
+      }
+
+      return form
+    }
   }
 </script>
